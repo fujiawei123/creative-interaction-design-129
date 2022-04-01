6e4a754b3aa5fa982f0752656e8d822792100306
fujiawei123 <vivian129131@cuc.edu.cn>
2022-04-01 11:21:59 +0800

    upup
    jiu

---
+++ script.js
@@ -0,0 +1,18 @@
+document.write("Helloiamxjy!");
+
+function sayhello(){
+  var response = prompt("what's your cat name?");
+  alert("help," + response + "I forgot to peep!");
+}
+
+function add(a,b){
+  return a + b;
+}
+console.log(add(20,30));
+console.log(add(200,300));
+
+function showTopic(){
+    var x = document.getElementById("momo");
+    x.style.fontSize = "25px";
+    x.style.color ="red";
+}
