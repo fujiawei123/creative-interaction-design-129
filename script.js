37efe759d14e8b8367fb3665fd70f020e63178da
FUJIAWEI <100392358+fujiawei123@users.noreply.github.com>
2022-04-13 17:30:38 +0800

    oo
    mm

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.write("Helloiamxjy!");
+document.write("Welcome to the guessing game");
 
 function sayhello(){
-  var response = prompt("what's your cat name?");
-  alert("help," + response + "I forgot to peep!");
+  var response = prompt("what's your name?");
+  alert("thanks god!!!  " + response + "  you're the only one who got it right");
 }
 
 function add(a,b){
@@ -12,7 +12,11 @@ console.log(add(20,30));
 console.log(add(200,300));
 
 function showTopic(){
-    var x = document.getElementById("momo");
-    x.style.fontSize = "25px";
-    x.style.color ="red";
+  var response = alert("wrong answer!please try again.");
+  var x = document.getElementById("momo");
+   x.style.fontSize = "25px";
+   x.style.color ="red";
+  var x = document.getElementById("dada");
+   x.style.fontSize = "25px";
+   x.style.color ="red";
 }
